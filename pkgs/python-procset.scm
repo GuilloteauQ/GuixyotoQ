298cf61fd191be972fa75b77e396f3c1ad56d873
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-11-11 21:49:24 +0100

    pybatsim ok

--- pkgs/python-procset.scm
+++ pkgs/python-procset.scm
@@ -21,8 +21,6 @@
 	    (base32
 	     "1cnmbw4sgl9156lgvakdkpjr7mgd2wasqz1zml9qzk29p705420z"))))
   (build-system python-build-system)
-  ; (native-inputs (list python-coverage python-interval-set python-pytest
-  ;                      python-pytest-cov))
   (synopsis "Toolkit to manage sets of closed intervals.")
   (description "Toolkit to manage sets of closed intervals.")
   (license gpl3)))
