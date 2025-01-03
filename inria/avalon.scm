c55b435e51f1cd507f20e3da9e08a1dcd297265d
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2025-01-03 22:27:21 +0100

    add execo @2.8.1

--- inria/avalon.scm
+++ inria/avalon.scm
@@ -1,9 +1,11 @@
 (define-module (inria avalon)
   #:use-module (guix)
+  #:use-module (guix packages)
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix build-system python)
   #:use-module (gnu packages)
+  #:use-module (gnu packages python-web)
   #:use-module (srfi srfi-1))
 
 (define-public python-concerto
@@ -23,3 +25,25 @@
     (synopsis "Preliminary implementation in Python 3 of the Concerto reconfiguration model.")
     (description "Preliminary implementation in Python 3 of the Concerto reconfiguration model.")
     (license license:gpl3)))
+
+(define-public python-execo
+    (package
+      (name "python-execo")
+      (version "2.8.1")
+      (source (origin
+          (method git-fetch)
+          (uri (git-reference
+            (url "https://gitlab.inria.fr/mimbert/execo")
+            (commit (string-append "v" version))))
+          (sha256
+            (base32
+              "1hqb1x8v7qrrznhzwr8rnf4s6jkz7yk5l1f46bqn3bfmy6jf1yjx"))))
+      (build-system python-build-system)
+      (inputs (list python-requests))
+      (home-page "https://gitlab.inria.fr/mimbert/execo")
+      (synopsis
+       "Python API for asynchronous control of local or remote, standalone or parallel, unix processes.")
+      (description
+       "Python API for asynchronous control of local or remote, standalone or parallel,
+    unix processes.")
+      (license license:gpl3)))
